82f31173d948ca33d45b43077815cc77cdfad142
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-10-13 22:03:17 +0530

    rjy added

--- src/cse123/Demo123.java
+++ src/cse123/Demo123.java
@@ -5,7 +5,7 @@ public class Demo123 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print("THIS IS DEMO OF GIT TO ECCLIIPSE");
+		System.out.print("THIS IS DEMO OF GIT TO ECCLIIPSE from DURGA PRASAD");
 	}
 
 }
