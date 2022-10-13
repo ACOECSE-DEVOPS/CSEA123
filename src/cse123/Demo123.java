52dfb286c7e07b6f307abd3ab57e9a88aec42185
Durga prasad <Durga prasad@LAPTOP-LN9V9DGH>
2022-10-13 22:00:31 +0530

    first commit

---
+++ src/cse123/Demo123.java
@@ -0,0 +1,11 @@
+package cse123;
+
+public class Demo123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("THIS IS DEMO OF GIT TO ECCLIIPSE");
+	}
+
+}
